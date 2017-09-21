69f62c52a3c0e53a5af25fda071a42f845c4e46c
Adrián Brito <adrbrpa1988@gmail.com>
2017-09-21 17:50:16 +0200

    Updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
+
+import md5 from 'crypto-md5';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    picture: string;
+    email: string;
 
-  }
+    constructor(public navCtrl: NavController) {
+        this.email = "";
+        this.picture = "https://www.gravatar.com/avatar/00000000000000000000000000000000?s=150";
+    }
+
+    getProfilePicture() {
+        if(this.validateEmail()) {
+            this.picture = "https://www.gravatar.com/avatar/" + md5(this.email.toLowerCase(), 'hex') + "?s=150";
+        }
+    }
+
+    // Taken from http://stackoverflow.com/a/46181/498479
+    validateEmail() {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(this.email);
+    }
 
 }
